d203d504504dccf0d37417c01e5663f8c6ea971c
wgabsi88 <wgabsi88@gmail.com>
2015-11-10 16:34:26 +0100

    first commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by jenkins on 27/04/15.
+//  Copyright (c) 2015 rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by jenkins on 27/04/15.
+//  Copyright (c) 2015 rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
